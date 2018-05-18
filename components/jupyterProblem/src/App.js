f496ff21d7cd3e366886df7ce96d9f73fa9f6def
Sarabjeet Singh <developer.up90@gmail.com>
2018-05-18 11:33:04 +0530

    Example Setup

--- components/jupyterProblem/src/App.js
+++ components/jupyterProblem/src/App.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
-// import Problem from './containers/Problem/Problem';
-import Problem from './components/problemViews/JupyterProblem';
-import Grid from 'material-ui/Grid';
-import ServiceURL from './components/serviceURL';
-import ProblemFile from './notebooks/problem.json';
-import SolutionFile from './notebooks/solution.json';
+import Example from './example';
 
 class App extends Component {
   constructor() {
@@ -19,22 +14,7 @@ class App extends Component {
     });
   };
   render() {
-    return (
-      <div>
-        <ServiceURL onPastingUrl={this.handleUrl} />
-        {this.state.problems && (
-          <div>
-            <h3>{this.state.problems.userId}</h3>
-            <h5>{this.state.problems.title}</h5>
-          </div>
-        )}
-        <Grid container justify="center">
-          <Grid item xs="4">
-            <Problem dispatch={() => {}} onChange={() => {}} problem={ProblemFile} solution={{}} />
-          </Grid>
-        </Grid>
-      </div>
-    );
+    return <Example />;
   }
 }
 
