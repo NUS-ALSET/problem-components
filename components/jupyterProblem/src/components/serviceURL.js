0afd6013741e87b1049804401776ab43c0c07e05
Sarabjeet Singh <developer.up90@gmail.com>
2018-05-22 11:18:37 +0530

    Added Button

--- components/jupyterProblem/src/components/serviceURL.js
+++ components/jupyterProblem/src/components/serviceURL.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Grid from 'material-ui/Grid';
 import TextField from 'material-ui/TextField';
+import Button from '@material-ui/core/Button';
 
 export default class ServiceURL extends Component {
   constructor() {
@@ -10,49 +11,48 @@ export default class ServiceURL extends Component {
       executionServiceUrl: 'https://usjqn1w4b6.execute-api.eu-central-1.amazonaws.com/Prod'
     }
   }
-  updateField = () => {
-    this.props.onPastingUrl(this.state.textField);
-  }
-  onSolutionRefreshClick = () => {
-    console.log('object')
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onPastingUrl(this.state);
   }
   render() {
     return (
-      <div>
-        <Grid container justify="center" >
-          <Grid item xs={10}>
-            <TextField value={this.state.problemUrl} label="Public Problem URL"
-              InputLabelProps={{
-                style: {
-                  top: 24,
-                  left: 24
-                }
-              }}
-              fullWidth
-              onChange={this.onSolutionFileChange}
-              style={{ padding: 24, position: "relative", }}
-            />
-          </Grid>
+      <Grid container justify='center'>
+        <Grid item xs={10}>
+            <div>
+            <form onSubmit={(e) =>this.handleSubmit(e)}>
+                  <TextField
+                    value={this.state.problemUrl} label="Public Problem URL"
+                    InputLabelProps={{
+                      style: {
+                        top: 24,
+                        left: 24
+                      }
+                    }}
+                    fullWidth
+                    style={{ padding: 24, position: "relative", }}
+                />
+                <br />
+                  <TextField
+                    value={this.state.executionServiceUrl} 
+                    label="Jupyter Execution Service Url"
+                    InputLabelProps={{
+                      style: {
+                        top: 24,
+                        left: 24
+                      }
+                    }}
+                    fullWidth
+                    style={{ padding: 24, position: "relative" }}
+                />
+                <br />
+                <Button type='submit' variant="raised" color="primary">
+                Submit
+                </Button>
+            </form>
+            </div>
         </Grid>
-        <Grid container justify="center" >
-          <Grid item xs={10}>
-            <TextField
-              value={this.state.executionServiceUrl} 
-              label="Jupyter Execution Service Url"
-              InputLabelProps={{
-                style: {
-                  top: 24,
-                  left: 24
-                }
-              }}
-              fullWidth
-              onChange={this.onSolutionFileChange}
-              style={{ padding: 24, position: "relative" }}
-            />
-          </Grid>
-        </Grid>
-
-      </div>
+    </Grid>
     )
   }
 }
--- components/jupyterProblem/src/example/index.js
+++ components/jupyterProblem/src/example/index.js
@@ -12,72 +12,65 @@ class Example extends React.Component {
       ProblemFile: {},
     };
     this._getProblem = this._getProblem.bind(this);
-    this._getStaticProblem = this._getStaticProblem.bind(this);
   }
   componentDidMount() {
     // this._getProblem();
-    this._getStaticProblem();
+    // this._getStaticProblem();
   }
   /**
    * Example function to get notebook's executed JSON data of given problem
    */
   _getProblem(json) {
-    fetch('https://usjqn1w4b6.execute-api.eu-central-1.amazonaws.com/Prod', {
-      method: 'POST',
-      body: JSON.stringify(Notebook),
-      headers: {
-        'user-agent': 'Mozilla/4.0 MDN Example',
-        'content-type': 'application/json',
-      },
-      mode: 'no-cors',
-    })
+    console.log('parent', json.problemUrl)
+    fetch(json.problemUrl)
       .then(response => response.json())
       .then(data => {
         this.setState({ ProblemFile: data });
       });
   }
-  _getStaticProblem() {
-    window.gapi.load("client:auth2", () => {
-      // 2. Initialize the JavaScript client library.
-      window.gapi.client
-        .init({
-          apiKey: "AIzaSyC27mcZBSKrWavXNhsDA1HJCeUurPluc1E",
-          // clientId and scope are optional if auth is not required.
-          clientId:
-            "765594031611-aitdj645mls974mu5oo7h7m27bh50prc.apps." +
-            "googleusercontent.com",
-          discoveryDocs: [
-            "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"
-          ]
-        })
-        .then(resolve => {
-          // 3. Initialize and make the API request.
-          console.log('init client', window.gapi.client.drive.files.get({
-            'fileId': '1bF3vk-jB5_Dmf7BeW3sXCtL-I1uP26ic',
-            'alt': 'media'
-          }).then((res) => console.log('res', res), (reason) => console.log('error', reason)))
-          // window.gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-          // updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-          // console.log('resolve', resolve)
-          // fetch('https://www.googleapis.com/drive/v3/files/1fu4vKrbwz4b4QzvHJzKo4CAPefIMGmd?alt=media')
-          //   .then(data => console.log('------', data))
-        });
-    })
-    fetch('https://raw.githubusercontent.com/walkwel/problem-components/master/jupyter-notebook-viewer/src/notebook.ipynb')
-    .then(data => data.json())
-    .then(dt =>{
-      this.setState({
-        ProblemFile: {
-          problemJSON: dt
-        },
-        problemFetched: true,
-      });
+  // _getStaticProblem() {
+  //   window.gapi.load("client:auth2", () => {
+  //     // 2. Initialize the JavaScript client library.
+  //     window.gapi.client
+  //       .init({
+  //         apiKey: "AIzaSyC27mcZBSKrWavXNhsDA1HJCeUurPluc1E",
+  //         // clientId and scope are optional if auth is not required.
+  //         clientId:
+  //           "765594031611-aitdj645mls974mu5oo7h7m27bh50prc.apps." +
+  //           "googleusercontent.com",
+  //         discoveryDocs: [
+  //           "https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"
+  //         ]
+  //       })
+  //       .then(resolve => {
+  //         // 3. Initialize and make the API request.
+  //         console.log('init client', window.gapi.client.drive.files.get({
+  //           'fileId': '1bF3vk-jB5_Dmf7BeW3sXCtL-I1uP26ic',
+  //           'alt': 'media'
+  //         }).then((res) => console.log('res', res), (reason) => console.log('error', reason)))
+  //         // window.gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+  //         // updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+  //         // console.log('resolve', resolve)
+  //         // fetch('https://www.googleapis.com/drive/v3/files/1fu4vKrbwz4b4QzvHJzKo4CAPefIMGmd?alt=media')
+  //         //   .then(data => console.log('------', data))
+  //       });
+  //   })
+  //   fetch('https://raw.githubusercontent.com/walkwel/problem-components/master/jupyter-notebook-viewer/src/notebook.ipynb')
+  //   .then(data => data.json())
+  //   .then(dt =>{
+  //     this.setState({
+  //       ProblemFile: {
+  //         problemJSON: dt
+  //       },
+  //       problemFetched: true,
+  //     });
   
-    })
+  //   })
 
-  }
+  // }
   render() {
     const { problemFetched, ProblemFile } = this.state;
+    console.log('rednder', this.state.ProblemFile)
     return (
       <div>
         <ServiceURL onPastingUrl={this._getProblem} />
